1cf59479e0764a40199859664e541739ad220d5d
Ricard Mallafre <rikitzzz@gmail.com>
2024-01-25 15:42:18 +0100

    fix(trpc-routers): simplified trpc routers logic

--- src/server/api/routers/categories.ts
+++ src/server/api/routers/categories.ts
@@ -1,4 +1,5 @@
 import { auth } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
 import { z } from 'zod';
 
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
@@ -6,16 +7,19 @@ import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 export const categoriesRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     const { userId } = auth();
-    if (!userId) return [];
-
-    return ctx.db.category.findMany({ where: { createdById: userId } });
-  }),
-
-  create: publicProcedure.input(z.object({ name: z.string().min(1) })).mutation(({ ctx, input }) => {
-    const { userId } = auth();
     if (!userId) throw new Error('Not authenticated');
 
-    return ctx.db.category.create({ data: { name: input.name, createdById: userId } });
+    return ctx.db.category.findMany({ where: { createdBy: { externalId: userId } } });
+  }),
+
+  create: publicProcedure.input(z.object({ name: z.string().min(1) })).mutation(async ({ ctx, input }) => {
+    const user = await currentUser();
+    if (!user) throw new Error('Not authenticated');
+    // if the user record in clerk does not have the id we use internally, we throw
+    if (!user.externalId) throw new Error('User has no externalId');
+
+    // this user record is from clerk, so the 'externalId' is our 'id'
+    return ctx.db.category.create({ data: { name: input.name, createdById: user.externalId } });
   }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,36 +1,48 @@
-import { auth, currentUser } from '@clerk/nextjs';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs';
+import type { Prisma, PrismaClient } from '@prisma/client';
+import type { DefaultArgs } from '@prisma/client/runtime/library';
 
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 
+export type DB = PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+
+const exists = async (db: DB): Promise<boolean> => {
+  const { userId } = auth();
+  if (!userId) throw new Error('Not authenticated');
+
+  const count = await db.user.count({ where: { externalId: userId } });
+  return count > 0;
+};
+
+const create = async (db: DB): Promise<ReturnType<DB['user']['create']>> => {
+  const user = await currentUser();
+  if (!user) throw new Error('No user authenticated, cannot create');
+
+  const userInDb = await db.user.create({
+    data: {
+      username: user.username,
+      externalId: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      imageUrl: user.imageUrl,
+      emailId: user.primaryEmailAddressId,
+    },
+  });
+
+  await clerkClient.users.updateUser(user.id, { externalId: userInDb.id });
+  return userInDb;
+};
+
+const get = async (db: DB): Promise<ReturnType<DB['user']['findUnique']>> => {
+  const { userId } = auth();
+  if (!userId) throw new Error('Not authenticated');
+
+  if (!(await exists(db))) return create(db);
+  return db.user.findUnique({ where: { externalId: userId } });
+};
+
 export const usersRouter = createTRPCRouter({
-  get: publicProcedure.query(({ ctx }) => {
-    const { userId } = auth();
-    if (!userId) throw new Error('Not authenticated');
-
-    return ctx.db.user.findUnique({ where: { externalId: userId } });
-  }),
-
-  create: publicProcedure.mutation(async ({ ctx }) => {
-    const user = await currentUser();
-    if (!user) throw new Error('No user');
-
-    return ctx.db.user.create({
-      data: {
-        username: user.username,
-        externalId: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        imageUrl: user.imageUrl,
-        emailId: user.primaryEmailAddressId,
-      },
-    });
-  }),
-
-  exists: publicProcedure.query(async ({ ctx }) => {
-    const { userId } = auth();
-    if (!userId) throw new Error('Not authenticated');
-
-    const count = await ctx.db.user.count({ where: { externalId: userId } });
-    return count > 0;
-  }),
+  get: publicProcedure.query(({ ctx }) => get(ctx.db)),
+  exists: publicProcedure.query(({ ctx }) => exists(ctx.db)),
+  create: publicProcedure.mutation(({ ctx }) => create(ctx.db)),
 });
