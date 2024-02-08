f084a01b3a42a1f952cd677d1c244def4d518d8d
Ricard Mallafre <rikitzzz@gmail.com>
2024-02-08 10:16:17 +0100

    refactor(users-router): inlined functions

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,53 +1,49 @@
 import { auth, clerkClient as clerk, currentUser } from '@clerk/nextjs';
-import type { DB } from '~/server/db';
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 import { log } from 'next-axiom';
 
-async function exists(db: DB): Promise<boolean> {
-  const { userId } = auth();
-  if (!userId) return false;
+export const usersRouter = createTRPCRouter({
+  exists: publicProcedure.query(async ({ ctx }) => {
+    const { userId } = auth();
+    if (!userId) return false;
 
-  const count = await db.user.count({ where: { externalId: userId } });
-  return count > 0;
-}
+    const count = await ctx.db.user.count({ where: { externalId: userId } });
+    return count > 0;
+  }),
+  create: publicProcedure.mutation(async ({ ctx }) => {
+    try {
+      const clerkUser = await currentUser();
+      if (!clerkUser) return null;
 
-async function create(db: DB): Promise<ReturnType<DB['user']['create']> | null> {
-  try {
-    const clerkUser = await currentUser();
-    if (!clerkUser) return null;
+      log.debug(`creating user ${clerkUser.id} in db`);
 
-    log.debug(`creating user ${clerkUser.id} in db`);
+      const userInDb = await ctx.db.user.create({
+        data: {
+          username: clerkUser.username,
+          externalId: clerkUser.id,
+          firstName: clerkUser.firstName,
+          lastName: clerkUser.lastName,
+          imageUrl: clerkUser.imageUrl,
+          email: clerkUser.emailAddresses[0]?.emailAddress,
+        },
+      });
 
-    const userInDb = await db.user.create({
-      data: {
-        username: clerkUser.username,
-        externalId: clerkUser.id,
-        firstName: clerkUser.firstName,
-        lastName: clerkUser.lastName,
-        imageUrl: clerkUser.imageUrl,
-        email: clerkUser.emailAddresses[0]?.emailAddress,
-      },
-    });
+      await clerk.users.updateUser(clerkUser.id, { externalId: userInDb.id });
+      log.debug('updated user in clerk');
 
-    await clerk.users.updateUser(clerkUser.id, { externalId: userInDb.id });
-    log.debug('updated user in clerk');
+      return userInDb;
+    } catch (e) {
+      log.error('error creating user', { e });
+      return null;
+    }
+  }),
+  get: publicProcedure.query(({ ctx }) => {
+    const { userId } = auth();
+    if (!userId) return null;
 
-    return userInDb;
-  } catch (e) {
-    log.error('error creating user', { e });
-    return null;
-  }
-}
-
-async function get(db: DB): Promise<ReturnType<DB['user']['findUnique']>> {
-  const { userId } = auth();
-  if (!userId) return null;
-
-  return db.user.findUnique({ where: { externalId: userId } });
-}
-
-async function sync(db: DB): Promise<void> {
-  try {
+    return ctx.db.user.findUnique({ where: { externalId: userId } });
+  }),
+  sync: publicProcedure.mutation(async ({ ctx }) => {
     const clerkUser = await currentUser();
     if (!clerkUser) return;
 
@@ -60,7 +56,7 @@ async function sync(db: DB): Promise<void> {
       imageUrl: clerkUser.imageUrl,
       email: clerkUser.emailAddresses[0]?.emailAddress,
     };
-    const userInDb = await db.user.upsert({
+    const userInDb = await ctx.db.user.upsert({
       create: userData,
       update: userData,
       where: { externalId: clerkUser.id },
@@ -70,14 +66,5 @@ async function sync(db: DB): Promise<void> {
       await clerk.users.updateUser(clerkUser.id, { externalId: userInDb.id });
       log.debug('updated user in clerk');
     }
-  } catch (e) {
-    log.error('error creating user', { e });
-  }
-}
-
-export const usersRouter = createTRPCRouter({
-  get: publicProcedure.query(({ ctx }) => get(ctx.db)),
-  exists: publicProcedure.query(({ ctx }) => exists(ctx.db)),
-  create: publicProcedure.mutation(({ ctx }) => create(ctx.db)),
-  sync: publicProcedure.mutation(({ ctx }) => sync(ctx.db)),
+  }),
 });
