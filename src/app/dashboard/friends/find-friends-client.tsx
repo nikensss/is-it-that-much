c64a4e4f91d674f17ff890544a9e166e89c702ce
Ricard Mallafre <rikitzzz@gmail.com>
2024-02-26 23:58:48 +0100

    fix(find-friends): improved the "find friends" list

--- src/app/dashboard/friends/find-friends-client.tsx
+++ src/app/dashboard/friends/find-friends-client.tsx
@@ -1,9 +1,19 @@
 'use client';
 
 import { AvatarIcon } from '@radix-ui/react-icons';
-import { Dot } from 'lucide-react';
+import { Dot, Loader2, UserRoundPlus, UserRoundX } from 'lucide-react';
 import { useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+import { Button } from '~/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from '~/components/ui/dialog';
 import { Input } from '~/components/ui/input';
 import { api } from '~/trpc/react';
 import type { RouterOutputs } from '~/trpc/shared';
@@ -15,7 +25,6 @@ export default function FindFriendsClient() {
   const query = api.users.find.useQuery(
     { search },
     {
-      initialData: [],
       enabled: search.length >= 3,
       onError: () => setUsers(() => []),
       onSuccess: (d) => {
@@ -46,16 +55,100 @@ export default function FindFriendsClient() {
 
 function User({ user }: { user: Exclude<RouterOutputs['users']['find'], null>[number] }) {
   return (
-    <div className="my-2 flex scale-[99.5%] items-center rounded-md border border-slate-100 p-4 transition hover:scale-100 hover:cursor-pointer hover:border-slate-900 hover:shadow-md">
-      <Avatar className="mr-4">
-        <AvatarImage src={user.imageUrl ?? ''} alt={`@${user.username}`} />
-        <AvatarFallback>
-          <AvatarIcon />
-        </AvatarFallback>
-      </Avatar>
-      <div>
-        {user.firstName} {user.lastName} {user.username ? `(@${user.username})` : ''}
+    <div className="my-2 flex items-center rounded-md border border-slate-100 p-4 hover:border-slate-900 hover:shadow-md">
+      <div className="flex items-center justify-center">
+        <Avatar className="mr-4">
+          <AvatarImage src={user.imageUrl ?? ''} alt={`@${user.username}`} />
+          <AvatarFallback>
+            <AvatarIcon />
+          </AvatarFallback>
+        </Avatar>
+        <div>
+          {user.firstName} {user.lastName} {user.username ? `(@${user.username})` : ''}
+        </div>
+      </div>
+      <div className="ml-auto flex gap-2">
+        <ButtonWithDialog
+          onConfirm={async () => {
+            await new Promise<void>((res) => {
+              console.log('sending friend request to', user.id);
+              setTimeout(() => {
+                console.log('friend request sent!');
+                res();
+              }, 1000);
+            });
+          }}
+          title="Send friend request"
+          description={`Are you sure you want to send a friend request to ${user.firstName} ${user.lastName}${user.username ? `(@${user.username})` : ''}?`}
+        >
+          <Button className="bg-slate-100 text-slate-900 hover:bg-slate-900 hover:text-slate-100">
+            <UserRoundPlus />
+          </Button>
+        </ButtonWithDialog>
+        <ButtonWithDialog
+          onConfirm={async () => {
+            await new Promise<void>((res) => {
+              console.log('blocking ', user.id);
+              setTimeout(() => {
+                console.log('user blocked!');
+                res();
+              }, 1000);
+            });
+          }}
+          title="Block user"
+          description={`Are you sure you want to block ${user.firstName} ${user.lastName}${user.username ? `(@${user.username})` : ''}?`}
+        >
+          <Button className="bg-red-100 text-slate-900 hover:bg-red-600 hover:text-slate-100">
+            <UserRoundX />
+          </Button>
+        </ButtonWithDialog>
       </div>
     </div>
+  );
+}
+
+function ButtonWithDialog({
+  children,
+  title,
+  description,
+  onConfirm,
+}: {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  onConfirm: () => Promise<void>;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isSending, setIsSending] = useState(false);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild onClick={() => setIsSending(false)}>
+        {children}
+      </DialogTrigger>
+      <DialogContent className="max-h-[80vh] overflow-y-auto overflow-x-hidden rounded-md max-sm:w-11/12">
+        <DialogHeader>{title}</DialogHeader>
+        <DialogDescription>{description}</DialogDescription>
+        <DialogFooter className="flex flex-col gap-4">
+          <Button
+            type="submit"
+            onClick={() => {
+              setIsSending(true);
+              onConfirm()
+                .catch(console.error)
+                .finally(() => setIsOpen(false));
+            }}
+            disabled={isSending}
+          >
+            {isSending ? <Loader2 className="animate-spin" /> : 'Confirm'}
+          </Button>
+          <DialogClose asChild>
+            <Button type="submit" variant="destructive" onClick={() => setIsOpen(false)}>
+              Cancel
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
