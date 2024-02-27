0813480f02a98e47967dd76350d3aeff7956b614
Ricard Mallafre <rikitzzz@gmail.com>
2024-02-27 10:53:36 +0100

    fix(settings-padding): padding in settings page was different from other pages

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -5,7 +5,7 @@ export default async function SettingsPage() {
   const user = await api.users.get.query();
 
   return (
-    <main className="flex flex-1 overflow-auto overflow-y-hidden bg-slate-100 p-4">
+    <main className="flex flex-1 overflow-auto overflow-y-hidden bg-slate-100 p-2">
       <section className="flex flex-grow flex-col rounded-md bg-white p-4 shadow-md">
         <header className="my-0.5 mb-1.5 flex h-12 items-center justify-center rounded-md bg-slate-900">
           <h2 className="text-lg font-bold text-slate-200">Settings</h2>
