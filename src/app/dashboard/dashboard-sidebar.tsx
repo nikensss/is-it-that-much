4aa5b968ce14e6e1291052cbfb465b3895680519
Ricard Mallafre <rikitzzz@gmail.com>
2024-02-28 12:16:15 +0100

    fix(sidebar): styling changes

--- src/app/dashboard/dashboard-sidebar.tsx
+++ src/app/dashboard/dashboard-sidebar.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { Button } from '~/components/ui/button';
 
 export default function DashboardSidebar() {
   return (
@@ -16,8 +15,13 @@ export default function DashboardSidebar() {
 
 function getButton(text: string, href: string) {
   return (
-    <Button asChild variant="ghost" className="select-none justify-start px-2 text-sm hover:bg-slate-300 md:text-lg">
-      <Link href={href}>{text}</Link>
-    </Button>
+    <Link
+      className="relative justify-start pl-2 md:text-lg md:after:absolute md:after:right-0 md:after:top-0.5 md:after:ml-0.5 md:after:block md:after:h-[20px] md:after:w-[20px] md:after:translate-x-[-1.5rem] md:after:opacity-0 md:after:transition-all md:after:content-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWFycm93LXJpZ2h0Ij48cGF0aCBkPSJNNSAxMmgxNCIvPjxwYXRoIGQ9Im0xMiA1IDcgNy03IDciLz48L3N2Zz4=')] md:hover:underline md:after:hover:translate-x-[-0.3rem] md:after:hover:opacity-100"
+      href={href}
+    >
+      <span className="text-sm font-medium md:text-xl">{text}</span>
+    </Link>
   );
 }
+
+// data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWFycm93LXJpZ2h0Ij48cGF0aCBkPSJNNSAxMmgxNCIvPjxwYXRoIGQ9Im0xMiA1IDcgNy03IDciLz48L3N2Zz4=
