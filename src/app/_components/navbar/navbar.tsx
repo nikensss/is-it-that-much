4433730bdd885fa7d90b317c139cf54aa0462e15
Ricard Mallafre <rikitzzz@gmail.com>
2024-03-01 16:02:21 +0100

    fix(home-link): does not look as goofy

--- src/app/_components/navbar/navbar.tsx
+++ src/app/_components/navbar/navbar.tsx
@@ -1,18 +1,15 @@
 import Link from 'next/link';
 import DesktopNavbar from '~/app/_components/navbar/navbar.desktop';
 import MobileNavbar from '~/app/_components/navbar/navbar.mobile';
-import { Button } from '~/components/ui/button';
 
 export function NavBar() {
   return (
     <header className="sticky top-0 z-50 flex h-16 items-center justify-between bg-white px-4 text-slate-50 md:justify-center">
       <div className="absolute bottom-1 left-1 right-1 top-1 z-0 rounded-md bg-slate-900"></div>
-      <Button asChild className="px-2 hover:bg-slate-300" variant="ghost">
-        <Link className="z-10 flex items-center justify-center" href="/">
-          <WalletIcon className="h-6 w-6" />
-          <span className="sr-only">Expense Tracker</span>
-        </Link>
-      </Button>
+      <Link className="z-10 flex items-center justify-center px-2" href="/">
+        <WalletIcon className="h-6 w-6" />
+        <span className="sr-only">Expense Tracker</span>
+      </Link>
       <nav className="z-10 hidden items-center justify-center md:ml-auto md:mr-2 md:flex md:gap-2">
         <DesktopNavbar />
       </nav>
