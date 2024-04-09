f9a20195a14c89d85bc49b98efae13c2777c3626
Ricard Mallafre <rikitzzz@gmail.com>
2024-04-09 23:47:05 +0200

    fix(dark-mode-toggle): dark mode toggle is now a switch instead of a menu

--- src/app/_components/mode-toggle.tsx
+++ src/app/_components/mode-toggle.tsx
@@ -1,50 +1,25 @@
 'use client';
 
-import * as React from 'react';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
 
-import { Button } from '~/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '~/components/ui/dropdown-menu';
-
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="border-primary-900 dark:border-primary-200 dark:bg-primary-900"
-        >
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        align="end"
-        className="bg-white text-primary-900 dark:border dark:border-primary-400 dark:bg-primary-900 dark:text-white"
-      >
-        <DropdownMenuItem
-          className="cursor-pointer hover:bg-primary-200 dark:hover:bg-gray-200/20"
-          onClick={() => setTheme('light')}
-        >
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className="cursor-pointer hover:bg-primary-200 dark:hover:bg-gray-200/20"
-          onClick={() => setTheme('dark')}
-        >
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <label
+      htmlFor="mode-toggle"
+      className="relative cursor-pointer rounded-lg border border-primary-900 p-2 dark:border-primary-200 dark:bg-primary-900"
+    >
+      <input
+        type="checkbox"
+        id="mode-toggle"
+        className="hidden"
+        onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      />
+      <SunIcon className="h-[1.2rem] w-[1.2rem] transition-all dark:-rotate-90 dark:scale-0 dark:opacity-0" />
+      <MoonIcon className="absolute top-[20%] h-[1.2rem] w-[1.2rem] translate-y-4 rotate-[120deg] opacity-0 transition-all duration-300 dark:translate-y-0 dark:opacity-100 " />
+      <span className="sr-only">Toggle theme</span>
+    </label>
   );
 }
