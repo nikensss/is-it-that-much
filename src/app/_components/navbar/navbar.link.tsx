d0f038db1a7c3ce06393e5f64302db7688a63591
Ricard Mallafre <rikitzzz@gmail.com>
2024-04-10 23:23:36 +0200

    fix: navbar button hover

--- src/app/_components/navbar/navbar.link.tsx
+++ src/app/_components/navbar/navbar.link.tsx
@@ -16,9 +16,9 @@ export default function NavBarLink({ children, href }: NavBarLinkProps) {
     <Link
       className={cn(
         pathname.startsWith(href)
-          ? 'border-primary-400 bg-primary-400/50 dark:border-primary-300 dark:bg-primary-500/20'
+          ? 'border-primary-400 bg-primary-400/30 dark:border-primary-300 dark:bg-primary-500/20'
           : 'border-transparent',
-        'self-stretch rounded-md border p-2 text-right font-medium transition-all md:inline-block md:px-2 lg:hover:bg-primary-300/20 dark:lg:hover:bg-primary-500/20',
+        'self-stretch rounded-md border p-2 text-right font-medium transition-all md:inline-block md:px-2 lg:hover:bg-primary-400/30 dark:lg:hover:bg-primary-500/20',
       )}
       href={href}
     >
