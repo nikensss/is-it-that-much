93a065402d75650326ad39abe0ef602042425824
Ricard Mallafre <rikitzzz@gmail.com>
2024-04-12 13:04:47 +0200

    fix: generalise user creation in db

--- src/app/api/webhooks/clerk/handlers/user/created.ts
+++ src/app/api/webhooks/clerk/handlers/user/created.ts
@@ -1,35 +1,8 @@
-import { clerkClient as clerk } from '@clerk/nextjs';
 import type { UserJSON } from '@clerk/nextjs/server';
 import { log } from 'next-axiom';
-import { db } from '~/server/db';
+import { createUserInDatabase } from '~/lib/utils';
 
 export async function processUserCreated(user: UserJSON): Promise<void> {
-  const email = user.email_addresses[0]?.email_address;
-  const emailParts = email?.split('@') ?? [];
-  emailParts.pop();
-  const emailLocalPart = emailParts.join('@').toLowerCase();
-
-  const userData = {
-    username: user.username,
-    externalId: user.id,
-    firstName: user.first_name,
-    lastName: user.last_name,
-    imageUrl: user.image_url,
-    email,
-    emailLocalPart,
-  };
-
-  if (await db.user.findUnique({ where: { externalId: user.id } })) {
-    log.debug('user already exists in db');
-    return;
-  }
-
-  log.debug('creating user in db');
-  const userInDb = await db.user.create({
-    data: userData,
-  });
-
-  log.debug('updating user in clerk');
-  const userUpdate = await clerk.users.updateUser(user.id, { externalId: userInDb.id });
-  log.debug('updated user in clerk', { userUpdate });
+  await createUserInDatabase(user.id);
+  log.debug('user created in db');
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,10 @@ import { getTimezoneOffset } from 'date-fns-tz';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import currencySymbolMap from 'currency-symbol-map/map';
+import { clerkClient as clerk } from '@clerk/nextjs';
+import { db } from '~/server/db';
+import { log } from 'next-axiom';
+import type { User } from '@prisma/client';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -38,4 +42,37 @@ export function displayTimezone(timezone: ReturnType<(typeof Intl)['supportedVal
 
 export function displayCurrency(currency: string) {
   return `${currency} (${currencySymbolMap[currency]})`;
+}
+
+export async function createUserInDatabase(userId: string): Promise<User> {
+  const user = await clerk.users.getUser(userId);
+  const email = user.emailAddresses[0]?.emailAddress;
+  const emailParts = email?.split('@') ?? [];
+  emailParts.pop();
+  const emailLocalPart = emailParts.join('@').toLowerCase();
+
+  const existingUser = await db.user.findUnique({ where: { email } });
+  if (existingUser) {
+    log.debug('user already exists in db');
+    return existingUser;
+  }
+
+  log.debug('creating user in db');
+  const userInDb = await db.user.create({
+    data: {
+      username: user.username,
+      externalId: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      imageUrl: user.imageUrl,
+      email,
+      emailLocalPart,
+    },
+  });
+
+  log.debug('updating user in clerk');
+  const userUpdate = await clerk.users.updateUser(user.id, { externalId: userInDb.id });
+  log.debug('updated user in clerk', { userUpdate });
+
+  return userInDb;
 }
