9fbb45ed90057c94c548788170b1274dc0c60173
Ricard Mallafre <rikitzzz@gmail.com>
2024-04-24 00:00:18 +0200

    fix: mobile navbar click auto-closes but does not indicate current location

--- src/app/_components/navbar/navbar-link.client.tsx
+++ src/app/_components/navbar/navbar-link.client.tsx
@@ -1,8 +1,4 @@
-'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { cn } from '~/lib/utils.client';
 
 export type NavBarLinkProps = {
   children: React.ReactNode;
@@ -10,16 +6,9 @@ export type NavBarLinkProps = {
 };
 
 export default function NavBarLink({ children, href }: NavBarLinkProps) {
-  const pathname = usePathname();
-
   return (
     <Link
-      className={cn(
-        pathname.startsWith(href)
-          ? 'border-primary-400 bg-primary-400/30 dark:border-primary-300 dark:bg-primary-500/20'
-          : 'border-transparent',
-        'self-stretch rounded-md border p-2 text-right font-medium transition-all md:inline-block md:px-2 lg:hover:bg-primary-400/30 dark:lg:hover:bg-primary-500/20',
-      )}
+      className="self-stretch rounded-md border border-transparent p-2 text-right font-medium transition-all md:inline-block md:px-2 lg:hover:bg-primary-400/30 dark:lg:hover:bg-primary-500/20"
       href={href}
     >
       <div>{children}</div>
