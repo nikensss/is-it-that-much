1df483a38204224e4c8d9bc73c204852a48ac065
Ricard Mallafre <rikitzzz@gmail.com>
2024-09-02 13:10:33 +0200

    fix: cursor pointer on group selector

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -107,7 +107,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-primary-100 focus:text-primary-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-primary-800 dark:focus:text-primary-50',
+      'relative flex w-full cursor-pointer select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-primary-100 focus:text-primary-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-primary-800 dark:focus:text-primary-50',
       className,
     )}
     {...props}
